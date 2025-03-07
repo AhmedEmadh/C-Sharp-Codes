576c6df0677148168157b1385b147c85c2cc7dc7
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-03-08 00:17:04 +0200

    Added More Examples

---
+++ AsynchronousProgramming/TaskClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskClass
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            // Create and run an asynchronous task
+            Task<int> resultTask = PerformAsyncOperation();
+
+            // Do some other work while waiting for the task to complete
+            Console.WriteLine("Doing some other work...");
+
+
+            // Wait for the task to complete and retrieve the result
+            int result = await resultTask;
+
+
+            // Process the result
+            Console.WriteLine($"Result: {result}");
+        }
+
+
+        static async Task<int> PerformAsyncOperation()
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(2000);
+
+            // Return a result
+            return 42;
+        }
+    }
+}
---
+++ Multithreading/ParameterizedThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParameterizedThread
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            // Note that your program is the main thread.
+
+
+            // Create a new thread and start it
+            Thread t1 = new Thread(() => ThreadMethod("ThreadMethod1"));
+            Thread t2 = new Thread(() => ThreadMethod("ThreadMethod2"));
+            t1.Start();
+            t2.Start();
+
+            t2.Join();
+
+            // Main thread continues its execution
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Main Thread: " + i);
+                Thread.Sleep(1000); // Sleep for 1 second
+            }
+            Console.ReadKey();
+        }
+        static void ThreadMethod(string name)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{name}: " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        
+    }
+}
