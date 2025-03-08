4199dee2067a343ce35e9e7093efec1d5e93b205
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-03-09 00:04:56 +0200

    Added Some Examples

---
+++ Data_Structures/List/ListExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+
+    internal class Program
+    {
+        public static void Print(List<int> Numbers)
+        {
+            foreach (int number in Numbers)
+            {
+                Console.Write(number);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Console.WriteLine("Count: " + numbers.Count);
+            Console.WriteLine("Capacity: " + numbers.Capacity);
+
+            numbers.AddRange(new List<int> { 9, 10, 11 });
+            numbers.InsertRange(5, new List<int> { 6, 7, 8 });
+
+            Print(numbers);
+            numbers.Remove(11);
+            numbers.RemoveAll(Numbers => Numbers % 2 == 0);
+            Print(numbers);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Data_Structures/List/ListLINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListLINQ
+{
+    internal class Program
+    {
+        public static void Print(List<int> Numbers)
+        {
+            foreach (int number in Numbers)
+            {
+                Console.Write(number);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine($"The List is {{{string.Join(",",list)}}}");
+            //LINQ
+            Console.WriteLine("The Sum Of The List is " + list.Sum());
+            Console.WriteLine("The Average Of The List is " + list.Average());
+            Console.WriteLine("The Maximum Of The List is " + list.Max());
+            Console.WriteLine("The Minimum Of The List is " + list.Min());
+            Console.WriteLine("The Count Of The List is " + list.Count());
+
+            //Filtering
+            Console.WriteLine("The Even Numbers Are: {" + string.Join(",", list.Where(n => n % 2 == 0))+"}");
+            Console.WriteLine("The Odd Numbers Are: {" + string.Join(",", list.Where(n => n % 2 != 0))+"}");
+            Console.WriteLine("The Numbers Greater Than 5 Are: {" + string.Join(",", list.Where(n => n > 5)) + "}");
+            Console.WriteLine("The Numbers Less Than 5 Are: {" + string.Join(",", list.Where(n => n < 5)) + "}");
+            //Sorting
+            Console.WriteLine("The List In Ascending Order: {" + string.Join(",", list.OrderBy(n => n)) + "}");
+            Console.WriteLine("The List In Descending Order: {" + string.Join(",", list.OrderByDescending(n => n)) + "}");
+            //Also Sorting
+            list.Sort();
+            list.Reverse();
+
+            
+            
+        }
+    }
+}
