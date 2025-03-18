b4b0dcd2a5c58a377909842bdb01f41a3f6ea874
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-03-18 20:32:03 +0200

    Moved APIs Folder Outside Data Structures Folder

---
+++ APIs/Win32_APIs/GetScreenResolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace GetScreenResolution
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(int nIndex);
+        static void Main(string[] args)
+        {
+            int screenWidth = GetSystemMetrics(0);
+            int screenHeight = GetSystemMetrics(1);
+            Console.WriteLine("Screen Width: " + screenWidth);
+            Console.WriteLine("Screen Height: " + screenHeight);
+            Console.ReadKey();
+        }
+    }
+}
